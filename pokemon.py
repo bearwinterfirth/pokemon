f2b663e91634ff84567547ffdefcb7647eca7908
Björn Winterfjord <bjorn.winterfjord@iths.se>
2024-09-23 10:16:22 +0200

    Read testpoints

--- pokemon.py
+++ pokemon.py
@@ -2,8 +2,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import random
 import numpy as np
+import re
 
 path1="datapoints.txt"
+path2="testpoints.txt"
 
 
 def get_datapoints_from_file():
@@ -15,5 +17,19 @@ def get_datapoints_from_file():
             main_list.append(row_list)
         return main_list
     
+
+
+def get_testpoints_from_file():
+    test_list=[]
+    with open(path2, "r") as testpoints:
+        for row in testpoints:
+            row=re.sub(r"[.]", "", row, count=1)
+            row=re.sub(r"[(),]", "", row)
+            row_list=row.strip().split(" ")
+            test_list.append(row_list)
+        return test_list
+
 main_list=get_datapoints_from_file()
+test_list=get_testpoints_from_file()
 print(main_list)
+print(test_list)
