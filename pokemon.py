df10e7a603e56715e15e3631ecabc411191c91c3
Björn Winterfjord <bjorn.winterfjord@iths.se>
2024-09-23 09:23:25 +0200

    Read datapoints

--- pokemon.py
+++ pokemon.py
@@ -1,1 +1,19 @@
-print("Hej")
+import matplotlib.pyplot as plt
+import pandas as pd
+import random
+import numpy as np
+
+path1="datapoints.txt"
+
+
+def get_datapoints_from_file():
+    main_list=[]
+    with open(path1, "r") as datapoints:
+        headers=datapoints.readline().strip()
+        for row in datapoints:
+            row_list=row.strip().split(",")
+            main_list.append(row_list)
+        return main_list
+    
+main_list=get_datapoints_from_file()
+print(main_list)
