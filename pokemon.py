47694300d5445f3972ac8411ab6001c3bc2bb33a
Björn Winterfjord <bjorn.winterfjord@iths.se>
2024-09-24 19:02:47 +0200

    Added list sorting

--- pokemon.py
+++ pokemon.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
-import pandas as pd
-import random
 import numpy as np
 import re
+
+path1="datapoints.txt"
+path2="testpoints.txt"
+pokemons=["Pichu", "Pikachu"]
 
 def get_datapoints_from_file():
     data_list=[]
@@ -24,8 +26,6 @@ def get_testpoints_from_file():
             test_list.append(row_list)
         return test_list
     
-
-
 def strings_to_numbers(list):
     for i in list:
         for j in range(len(i)):
@@ -34,7 +34,7 @@ def strings_to_numbers(list):
 
 def plot_data_points():
     colors=["r", "b"]
-    for j in main_list:
+    for j in data_list:
         plt.plot(j[0],j[1],f"{colors[int(j[2])]}.")
 
 def plot_test_points():
@@ -42,19 +42,15 @@ def plot_test_points():
         plt.plot(j[1], j[2], "g*")
     
 def calculate_distance(x):
-    pichu_distance_list=[]
-    pikachu_distance_list=[]
-    for data_point in main_list:
-        distance=np.sqrt(np.square(x[1]-data_point[0])+np.square(x[2]-data_point[1]))
-        if data_point[2]==0:
-            pichu_distance_list.append(distance)
-        else:
-            pikachu_distance_list.append(distance)
-    if min(pichu_distance_list) < min(pikachu_distance_list):
-        return [x[0], 0]
-    else:
-        return [x[0], 1]
+    for data_point in data_list:
+        distance=np.sqrt(np.square(x[1] - data_point[0]) + np.square(x[2] - data_point[1]))
+        data_point.append(distance)
 
+def find_nearest():
+    data_list.sort(key = lambda x: x[-1])
+    type=int(data_list[0][2])
+    print(f"Pokemon number {int(test_point[0])} is classified as {pokemons[type]}.")
+    
 def ask_for_width():
     while True:
         x=(input("Width (cm)? "))
@@ -74,16 +70,9 @@ def ask_for_height():
                 break 
         print("Height must be a number between 10 and 50!")
     return x
-    
 
-path1="datapoints.txt"
-path2="testpoints.txt"
-classification=[]
-pokemons=["Pichu", "Pikachu"]
-
-main_list=get_datapoints_from_file()
-main_list=shuffle_list(main_list)
-main_list=strings_to_numbers(main_list)
+data_list=get_datapoints_from_file()
+data_list=strings_to_numbers(data_list)
 
 test_list=get_testpoints_from_file()
 test_list=strings_to_numbers(test_list)
@@ -93,18 +82,28 @@ plot_test_points()
 plt.show()
 
 for test_point in test_list:
-    result=calculate_distance(test_point)
-    classification.append(result)
+    calculate_distance(test_point)
+    find_nearest()
 
-for j in classification:
-    print (f"Pokemon # {j[0]} is classified as {pokemons[j[1]]}.")
+print("\nNow it's your turn! Please enter width and height for your pokemon!")
+print("The unit is cm, and your values should be between 10 cm and 50 cm.")
 
-print("Now it's your turn! Please enter width and height for your pokemon!")
-print("The unit is cm, and your values should be between 10 cm and 40 cm.")
-width=ask_for_width()
-height=ask_for_height()
-result=calculate_distance([5,width,height])
-print (f"Your pokemon is classified as {pokemons[result[1]]}.")
+pokemon_number=5
+
+while True:
+    width=ask_for_width()
+    height=ask_for_height()
+
+    test_point=(pokemon_number,width,height)
+    calculate_distance(test_point)
+    find_nearest()
+
+    try_again=input("\nDo you have more pokemons to classify? (y/n) ")
+    if try_again != "y":
+        break
+    pokemon_number += 1    
+
+
 
 
 
